0a2aa540566035341e14eb4bbcccc03c086675b8
Hrvoje Janđel <hrjandje@vsite.hr>
2024-02-13 16:22:21 +0100

    Add project files.

---
+++ seminar/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace seminar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.tmrSeconds = new System.Windows.Forms.Timer(this.components);
+            this.tmrMinutes = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSet
+            // 
+            this.btnSet.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSet.Location = new System.Drawing.Point(54, 333);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(85, 47);
+            this.btnSet.TabIndex = 0;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnStart.Location = new System.Drawing.Point(312, 333);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 46);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Visible = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnReset.Location = new System.Drawing.Point(478, 333);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(85, 46);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTime.AutoSize = true;
+            this.lblTime.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 120F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(53, 7);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(595, 226);
+            this.lblTime.TabIndex = 3;
+            this.lblTime.Text = "00:00";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputBox
+            // 
+            this.inputBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(173, 335);
+            this.inputBox.Multiline = true;
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(64, 46);
+            this.inputBox.TabIndex = 4;
+            this.inputBox.TextChanged += new System.EventHandler(this.inputBox_TextChanged);
+            // 
+            // tmrSeconds
+            // 
+            this.tmrSeconds.Interval = 1000;
+            this.tmrSeconds.Tick += new System.EventHandler(this.tmrSeconds_Tick);
+            // 
+            // tmrMinutes
+            // 
+            this.tmrMinutes.Interval = 60000;
+            this.tmrMinutes.Tick += new System.EventHandler(this.tmrMinutes_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(699, 450);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnSet);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Countdown";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Timer tmrSeconds;
+        private System.Windows.Forms.Timer tmrMinutes;
+    }
+}
+
---
+++ seminar/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace seminar
+{
+    public partial class Form1 : Form
+    {
+        private int totalMinutes;
+        private int seconds;
+
+        public SoundPlayer alertSound = new SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            SetStartValues();
+        }
+
+        private void SetStartValues()
+        {
+            if (int.TryParse(inputBox.Text, out int result))
+            {
+                totalMinutes = result - 1;
+                seconds = 60;
+                lblTime.Text = result.ToString();
+                btnStart.Visible = true;
+            }
+            else
+            {
+                lblTime.Text = "00:00";
+                btnStart.Visible = false;
+            }
+        }
+        private void inputBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void tmrSeconds_Tick(object sender, EventArgs e)
+        {
+            --seconds;
+            if (seconds >= 0)
+            {
+                PrintTime();
+            }
+            else
+            {
+                --totalMinutes;
+                seconds = 59;
+                PrintTime();
+            }
+
+            if (totalMinutes <= 4)
+            {
+               
+                if (seconds % 2 == 0)
+                {
+                    lblTime.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    lblTime.BackColor = Color.Red;
+                }
+            }
+        }
+
+        private void PrintTime()
+        {
+            lblTime.Text = $"{totalMinutes}:{seconds}";
+        }
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            tmrSeconds.Start();
+            tmrMinutes.Start();
+        }
+
+        private void tmrMinutes_Tick(object sender, EventArgs e)
+        {
+            if (totalMinutes == 5)
+            {
+                PlayAlarmSound();
+            }
+            if (totalMinutes == 0)
+            {
+                tmrSeconds.Stop();
+                tmrMinutes.Stop();
+                seconds = 0;
+                PrintTime();
+                PlayAlarmSound();
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        { 
+            tmrMinutes.Stop();
+            tmrSeconds.Stop();
+            lblTime.Text = "00:00";
+            SetStartValues();
+            ResetColors();
+        }
+        private void PlayAlarmSound()
+        {
+            alertSound.SoundLocation = "C:\\Windows\\Media\\Alarm01.wav";
+            alertSound.Play();
+        }
+        private void ResetColors()
+        {
+            lblTime.BackColor = Color.Transparent;
+        }
+       
+    }
+}
+    
+
+
+
